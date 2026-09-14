37bfd5555249b9e5b305dbf590525fa40ed12053
WANGYUQI <3980817679@qq.com>
2026-09-14 19:43:35 +0800

    上传力扣15、更新README.md

---
+++ 2026.9.14-2 双指针-三数之和 力扣15/代码.cpp
@@ -0,0 +1,41 @@
+//2026.9.14
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> res;
+        sort(nums.begin(), nums.end());
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (nums[i] > 0) {
+                return res;
+            }
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.size() - 1;
+            while (right > left) {
+                if (nums[i] + nums[left] + nums[right] > 0) {
+                    right--;
+                }
+                else if (nums[i] + nums[left] + nums[right] < 0) {
+                    left++;
+                }
+                else {//´ËÊ±nums[i] + nums[left] + nums[right] = 0
+                    res.push_back(vector<int> {nums[i], nums[left], nums[right]});
+                    while (right > left && nums[left] == nums[left + 1]) {
+                        left++;
+                    }
+                    while (right > left && nums[right] == nums[right - 1]) {
+                        right--;
+                    }
+                    left++;
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+};
